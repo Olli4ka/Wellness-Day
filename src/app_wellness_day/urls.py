107a7dd208c1115a98bfd6a3884814f52d4d8cc4
Olga Panayot <panayot.olga@gmail.com>
2026-05-20 17:59:59 +0300

    feat: add URL routes for home and register views

---
+++ src/app_wellness_day/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import home_view, register_view
+
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
+]
