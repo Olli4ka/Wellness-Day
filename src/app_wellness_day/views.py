bb1ae1ed597e72fd6f1d1b1f6b495d5ccac2786b
Olga Panayot <panayot.olga@gmail.com>
2026-05-20 17:48:17 +0300

    feat: add view functions for home and register

--- src/app_wellness_day/views.py
+++ src/app_wellness_day/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def register_view(request):
+
+    if request.method == 'POST':
+
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+
+            messages.success(
+                request,
+                f'Congratulations, {form.cleaned_data["full_name"]}! You have successfully registered for the conference!🎉'
+            )
+
+            return redirect('home')
+
+    else:
+        form = RegistrationForm()
+
+    return render(
+        request,
+        'register.html',
+        {'form': form}
+    )
